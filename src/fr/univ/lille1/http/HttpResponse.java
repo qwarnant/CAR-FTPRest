7c4c140af44ceeb6019829f3ebe76ee69afb63db
Quentin Warnant <qwarnant@gmail.com>
2015-03-01 19:52:48 +0100

    add remove file/get/remove folder

---
+++ src/fr/univ/lille1/http/HttpResponse.java
@@ -0,0 +1,14 @@
+package fr.univ.lille1.http;
+
+/**
+ * Created by Warnant on 01-03-15.
+ */
+public class HttpResponse {
+
+    public static final int CODE_STATUS_OK = 200;
+    public static final String MSG_STATUS_OK = "The request was successful.";
+
+    public static final int CODE_FORBIDDEN = 403;
+    public static final String MSG_FORBIDDEN = "Access denied. Wrong username or password.";
+
+}
--- src/fr/univ/lille1/service/FtpService.java
+++ src/fr/univ/lille1/service/FtpService.java
@@ -1,59 +1,177 @@
 package fr.univ.lille1.service;
 
+import fr.univ.lille1.http.HttpResponse;
 import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPFile;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.SocketException;
+
 
 @Path("/ftp")
 public class FtpService {
 
-	private FTPClient ftpClient = new FTPClient();
+    private static final String FTP_SERVER_IP = "127.0.0.1";
+    private static final int FTP_SERVER_PORT = 1024;
+
+    private static final String FTP_USER_ANONYMOUS = "anonymous";
+    private static final String FTP_PASS_ANONYMOUS = "anonymous";
+
+    private FTPClient client;
 
     public FtpService() {
-        ftpClient = new FTPClient();
-		try {
-			ftpClient.connect("127.0.0.1", 1024);
-			ftpClient.login("johndoe", "foo");
-			ftpClient.setKeepAlive(true);
+        this.client = new FTPClient();
+        try {
+            this.client.connect(FTP_SERVER_IP, FTP_SERVER_PORT);
 
-		} catch (SocketException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+            // Anonymous login by default
+            this.client.login(FTP_USER_ANONYMOUS, FTP_PASS_ANONYMOUS);
+            this.client.setKeepAlive(true);
 
-	}
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	@GET
-	@Produces("text/html")
-	public String list() {
-		String texte = "";
-		try {
+    }
 
-			for(FTPFile file : ftpClient.listFiles()){
-				texte += file.getName() + "<br>";
-			}
-			
-		} catch (IOException e) {
-			texte = e.getMessage();
-		}
-		return texte;
-	}
+    @POST
+    @Path("/connect")
+    @Produces("text/html")
+    public Response connect(@FormParam("username") String username, @FormParam("password") String password) {
+        System.out.println("start connect");
+        try {
+            // Try to connect the user
+            boolean result = this.client.login(username, password);
+            System.out.println("Result login : " + result);
 
-	// @GET
-	// @Path("/book/{isbn}")
-	// public String getBook( @PathParam("isbn") String isbn ) {
-	// return "Book: "+isbn;
-	// }
-	//
-	// @GET
-	// @Path("{var: .*}/stuff")
-	// public String getStuff( @PathParam("var") String stuff ) {
-	// return "Stuff: "+stuff;
-	// }
+            // If the connection failed, reconnect the user as anonymous
+            if (!result) {
+                return Response.status(HttpResponse.CODE_FORBIDDEN).entity(HttpResponse.MSG_FORBIDDEN).build();
+            }
+
+            return Response.status(HttpResponse.CODE_STATUS_OK).entity(HttpResponse.MSG_STATUS_OK).build();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("end connect");
+
+
+        return null;
+    }
+
+
+    @POST
+    @Path("/file")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces("text/html")
+    public Response postFile(@Context HttpServletRequest request) {
+
+        try {
+            System.out.println("" + request.getParts());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return Response.status(HttpResponse.CODE_STATUS_OK).entity("postFile").build();
+
+    }
+
+
+    @GET
+    @Path("/file/{filename}")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response getFile(@PathParam("filename") String filename) {
+        File file = new File(System.getProperty("java.io.tmpdir") + filename);
+
+        try {
+            OutputStream os = new FileOutputStream(file);
+
+            System.out.println(file.getName());
+
+            boolean status = this.client.retrieveFile(filename, os);
+            os.flush();
+
+            if (!status) {
+                return Response.status(404).entity("File not found.").build();
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        Response response = Response.ok(file, MediaType.APPLICATION_OCTET_STREAM)
+                .header("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"") //optional
+                .build();
+
+        System.out.println(file.length());
+        
+        // Delete the temp file
+        file.delete();
+        
+        return response;
+    }
+
+    @DELETE
+    @Path("/file/{filename}")
+    @Produces("text/html")
+    public Response deleteFile(@PathParam("filename") String filename) {
+
+        try {
+
+            boolean status = this.client.deleteFile(filename);
+
+            if (!status) {
+                return Response.status(404).entity("File not found.").build();
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return Response.ok().build();
+    }
+
+    @DELETE
+    @Path("/folder/{foldername}")
+    @Produces("text/html")
+    public Response removeFolder(@PathParam("foldername") String foldername) {
+
+        try {
+
+            boolean status = this.client.removeDirectory(foldername);
+
+            if (!status) {
+                return Response.status(404).entity("Folder not found.").build();
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return Response.ok().build();
+    }
+
+    @GET
+    @Produces("text/html")
+    public Response listFolder() {
+        System.out.println("Start list");
+        String texte = "";
+
+        //TODO
+
+        System.out.println("End list : " + texte);
+
+        return Response.status(HttpResponse.CODE_STATUS_OK).entity(texte).build();
+    }
 }
